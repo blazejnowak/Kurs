ddfc4133eebbf6aad678807a4364c8288bdb313d
Stanisław Bugalski <git@bugalski.pro>
2021-07-03 20:46:47 +0200

    Support for running as Windows Service

--- dotnet/aspnetcore31/webappWithTests-KAD/Application/aspnet-core-dotnet-core/Program.cs
+++ dotnet/aspnetcore31/webappWithTests-KAD/Application/aspnet-core-dotnet-core/Program.cs
@@ -7,18 +7,30 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using aspnet_core_dotnet_core.Services;
 
 namespace aspnet_core_dotnet_core
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ServiceA>();
+                    services.AddHostedService<ServiceB>();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
