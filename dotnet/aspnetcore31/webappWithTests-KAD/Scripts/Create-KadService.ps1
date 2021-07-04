f61c1be7bdd35904d1d1821a82fed7a448541510
Stanisław Bugalski <git@bugalski.pro>
2021-07-04 19:07:52 +0200

    add firewall rule

--- dotnet/aspnetcore31/webappWithTests-KAD/Scripts/Create-KadService.ps1
+++ dotnet/aspnetcore31/webappWithTests-KAD/Scripts/Create-KadService.ps1
@@ -3,11 +3,14 @@
     Demo script used to create Windows service for Kurs Azure DevOps's demo .NET Core app.
 .DESCRIPTION
     Script creates Windows service with .exe path. Script takes 3 parameters:
-    - Service Name (serviceName) - used to create and start service
-    - Application Path - folder path where application is stored
-    - Exe Name - application name with exstension
+    - Service Name ($serviceName) - used to create and start service
+    - Application Path ($appPath) - folder path where application is stored
+    - Exe Name ($exeName) - application name with exstension
+    - No firewall Rule ($noFirewallRule) - remove windows firewall rule
 .EXAMPLE
     PS C:\> .\Create-KadService.ps1 -appPath "C:\tmp\KursAzureDevOps\dotnet\aspnetcore31\webappWithTests-KAD\Application\aspnet-core-dotnet-core\bin\Debug\netcoreapp3.1" -exeName "aspnet-core-dotnet-core.exe" -serviceName "SVC-KAD-Demo"
+
+    PS C:\> .\Create-KadService.ps1 -appPath "C:\tmp\KursAzureDevOps\dotnet\aspnetcore31\webappWithTests-KAD\Application\aspnet-core-dotnet-core\bin\Debug\netcoreapp3.1" -exeName "aspnet-core-dotnet-core.exe" -serviceName "SVC-KAD-Demo" -noFirewallRule
 #>
 
 [CmdletBinding()]
@@ -17,7 +20,9 @@ param (
     [Parameter(Mandatory=$true)][string][ValidateNotNullOrEmpty()]
     $appPath,
     [Parameter(Mandatory=$true)][string][ValidateNotNullOrEmpty()]
-    $exeName
+    $exeName,
+    [switch]
+    $noFirewallRule
 )
 
 # Variables
@@ -34,3 +39,11 @@ if (Get-Service $serviceName -ErrorAction SilentlyContinue) {
 # Run service
 Write-Host "Starting service: $serviceName" -ForegroundColor "Green"
 Start-Service "$serviceName" -PassThru
+
+# Set Firewall Rule
+if (! $noFirewallRule) {
+    New-NetFirewallRule -Name "$serviceName-In" -DisplayName "Allow Inbound $serviceName" -Direction Inbound -Program "$appExePath" -RemoteAddress LocalSubnet -Action Allow -ErrorAction Continue
+}
+else {
+    Remove-NetFirewallRule -Name "$serviceName-In" -ErrorAction Continue
+}
